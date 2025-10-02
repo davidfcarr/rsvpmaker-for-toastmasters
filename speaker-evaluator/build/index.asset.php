aa0cd51795aebb15e5f7efdea8079db8f5d0e374
davidfcarr <david@carrcommunications.com>
2025-10-01 21:47:05 -0400

    npm update speaker-evaluator

--- speaker-evaluator/build/index.asset.php
+++ speaker-evaluator/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bf0a2e0ecadd425a857b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '26ce9e0ce66fb8ce6ede');
